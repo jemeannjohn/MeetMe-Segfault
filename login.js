c7e79646263ae0980fa01c47c81336f122d3d645
Yousuf Hussain <yousuf.1991@hotmail.com>
2015-10-01 00:36:55 -0700

    -Changed database access from client to sever side. -Removed unnecessary javascript code PS- I've deployed it on the website with google login working

--- login.js
+++ login.js
@@ -4,19 +4,7 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         tasks: function () {
-            if (Session.get("hideCompleted")) {
-                // If hide completed is checked, filter tasks
-                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-            } else {
-                // Otherwise, return all of the tasks
-                return Tasks.find({}, {sort: {createdAt: -1}});
-            }
-        },
-        hideCompleted: function () {
-            return Session.get("hideCompleted");
-        },
-        incompleteCount: function () {
-            return Tasks.find({checked: {$ne: true}}).count();
+            return Tasks.find({}, {sort: {createdAt: -1}});
         }
     });
     Template.body.events({
@@ -28,36 +16,49 @@ if (Meteor.isClient) {
             var text = event.target.text.value;
 
             // Insert a task into the collection
-            Tasks.insert({
-                text: text,
-                createdAt: new Date() // current time
-            });
+            Meteor.call("addTask", text);
 
             // Clear form
             event.target.text.value = "";
-        },
-        "change .hide-completed input": function (event) {
-            Session.set("hideCompleted", event.target.checked);
         }
     });
     Template.task.events({
         "click .toggle-checked": function () {
             // Set the checked property to the opposite of its current value
-            Tasks.update(this._id, {
-                $set: {checked: !this.checked}
-            });
+            Meteor.call("setChecked", this._id, !this.checked);
         },
         "click .delete": function () {
-            Tasks.remove(this._id);
+            Meteor.call("deleteTask", this._id);
         }
-    });
-    Tasks.insert({
-        text: text,
-        createdAt: new Date(),            // current time
-        owner: Meteor.userId(),           // _id of logged in user
-        username: Meteor.user().username  // username of logged in user
     });
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_ONLY"
     });
 }
+Meteor.methods({
+    addTask: function (text) {
+        // Make sure the user is logged in before inserting a task
+        if (!Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
+        var name = Meteor.user().username;
+        if (name === undefined) {
+            var queryDocument = Meteor.users.findOne(
+                {_id: Meteor.userId()},
+                {'profile.name': 1});
+            name = queryDocument.profile.name;
+        }
+        Tasks.insert({
+            text: text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: name
+        });
+    },
+    deleteTask: function (taskId) {
+        Tasks.remove(taskId);
+    },
+    setChecked: function (taskId, setChecked) {
+        Tasks.update(taskId, {$set: {checked: setChecked}});
+    }
+});
