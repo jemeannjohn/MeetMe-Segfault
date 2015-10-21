0cebeebd2b8cabe0738d35ac2b93e3a3cbe4fada
Vishal Johri <vjohri@asu.edu>
2015-10-21 16:03:08 -0700

    building MeetingDetails collection in mongoDB

--- both/collections/collections.js
+++ both/collections/collections.js
@@ -1,6 +1,25 @@
 var Collections = {};
 
-Meteor.isClient && Template.registerHelper("Collections", Collections);
+Template.registerHelper("Collections", Collections);
 
 People = Collections.People = new Mongo.Collection("People");
 People.attachSchema(Schemas.Person);
+
+MeetingDetails = Collections.MeetingDetails = new Mongo.Collection("MeetingDetails");
+MeetingDetails.attachSchema(Schemas.Meeting);
+
+Meteor.publish(null, function () {
+    return MeetingDetails.find();
+});
+MeetingDetails.allow({
+    insert: function () {
+        console.log("inside insert");
+        return true;
+    },
+    update: function () {
+        return true;
+    },
+    remove: function () {
+        return true;
+    }
+});
